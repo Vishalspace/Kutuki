d3b6cc6774afe89d06d7122725cae31862b48c0f
VishalY <vishaly.space@gmail.com>
2022-02-25 23:58:42 +0530

    clean up video player

--- app/src/main/java/com/kutuki/VideosActivity.kt
+++ app/src/main/java/com/kutuki/VideosActivity.kt
@@ -14,6 +14,7 @@ import com.kutuki.databinding.VideosActivityBinding
 import com.kutuki.model.Category
 import com.kutuki.model.Video
 import com.kutuki.model.VideosMap
+import com.kutuki.service.VideoPlayer
 import com.kutuki.service.VideoService
 import com.kutuki.ui.VideosAdapter
 import com.kutuki.utils.Logger
@@ -26,11 +27,11 @@ import javax.inject.Inject
 class VideosActivity : AppCompatActivity() {
     @Inject
     lateinit var videoService: VideoService
+    @Inject
+    lateinit var videoPlayer: VideoPlayer
     private lateinit var adapter: VideosAdapter
     private lateinit var binding: VideosActivityBinding
     private val compositeDisposable = CompositeDisposable()
-
-    private lateinit var simpleExoPlayer: SimpleExoPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,35 +42,23 @@ class VideosActivity : AppCompatActivity() {
     }
 
     private fun initUi() {
-        adapter = VideosAdapter(this) {
-            simpleExoPlayer.stop()
-            simpleExoPlayer.release()
-            updateMainVideoSource(it.second)
-        }
+        adapter = VideosAdapter(this) { onVideoClicked(it.second) }
         binding.videosRecyclerView.layoutManager =
             LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         binding.videosRecyclerView.adapter = adapter
         binding.vaBackButton.setOnClickListener { onBackPressed() }
+        binding.exoplayerView.player = videoPlayer.player
     }
 
-    private fun updateMainVideoSource(video: Video) {
+    private fun onVideoClicked(video: Video) {
         Glide.with(this).load(video.thumbnailURL).into(binding.currentVideoThumbnail)
-        initPlayer(video)
+        videoPlayer.play(video)
     }
 
     private fun onVideosLoaded(videosMap: VideosMap) {
         adapter.updateData(videosMap)
-        adapter.firstItem()?.let { updateMainVideoSource(it) }
+        adapter.firstItem()?.let { onVideoClicked(it) }
     }
-
-    private fun initPlayer(video: Video) {
-        simpleExoPlayer = SimpleExoPlayer.Builder(this).build()
-        binding.exoplayerView.player = simpleExoPlayer
-        val mediaItem = MediaItem.fromUri(video.videoURL)
-        simpleExoPlayer.setMediaItem(mediaItem)
-        simpleExoPlayer.prepare()
-    }
-
 
     override fun onResume() {
         super.onResume()
@@ -83,21 +72,13 @@ class VideosActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
         compositeDisposable.clear()
-        simpleExoPlayer.pause()
-        simpleExoPlayer.playWhenReady = false
-    }
-
-    override fun onStop() {
-        super.onStop()
-        simpleExoPlayer.pause()
-        simpleExoPlayer.playWhenReady = false
+        videoPlayer.pause()
     }
 
     override fun onDestroy() {
         super.onDestroy()
         window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-        simpleExoPlayer.stop()
-        simpleExoPlayer.clearMediaItems()
+        videoPlayer.stop()
     }
 
     companion object {
--- app/src/main/java/com/kutuki/service/VideoPlayer.kt
+++ app/src/main/java/com/kutuki/service/VideoPlayer.kt
@@ -1,3 +1,26 @@
 package com.kutuki.service
 
-class VideoPlayer
+import android.content.Context
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.kutuki.model.Video
+import javax.inject.Inject
+
+class VideoPlayer @Inject constructor(context: Context) {
+    val player = SimpleExoPlayer.Builder(context).build().apply { playWhenReady = true }
+
+    fun play(video: Video) {
+        player.setMediaItem(MediaItem.fromUri(video.videoURL))
+        player.prepare()
+    }
+
+    fun pause() {
+        player.pause()
+    }
+
+    fun stop() {
+        player.stop()
+        player.release()
+        player.clearMediaItems()
+    }
+}
